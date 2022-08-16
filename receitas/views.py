c76a65e4a5b2fc236271d0d4a9dbd04494484469
michail salata <michailsalata@gmail.com>
2022-08-16 19:21:29 -0300

    Paginaçao no templetes

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,get_list_or_404,get_object_or_404
 from .models import Receita
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
     
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    paginator = Paginator(receitas, 3)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
 
     dados = {
-        'receitas' : receitas
+        'receitas' : receitas_por_pagina
     }
     return render(request, 'receitas/index.html', dados)
 
